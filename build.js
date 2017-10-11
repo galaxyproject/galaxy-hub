72b0592f466eae3f89f8f702e183c2aff9c239c6
Dannon Baker <dannon.baker@gmail.com>
2017-10-11 15:17:53 -0400

    Quick pass implementation of file staging for json feeds.

--- build.js
+++ build.js
@@ -155,6 +155,21 @@ let subs = function(files, metalsmith, done) {
     return done();
 };
 
+
+let file_staging = function(files, metalsmith, done) {
+    // We want to use metadata.md (which turns into metadata.html after
+    // processing) as the topic index.
+    for (let k in files) {
+        let v = files[k];
+        if (Array.from(v.collection).includes('json_feed')) {
+            files[k.replace('.html', '.json')] = files[k];
+            delete files[k];
+        }
+    }
+    return done();
+};
+
+
 // Extend `marked.Renderer` to increase all heading levels by 1 since we reserve
 // h1 for the page title. Will be passed to `metalsmith-markdown` plugin.
 class Renderer extends marked.Renderer {
@@ -257,6 +272,8 @@ let ms = metalsmith(__dirname)
             _: require('lodash')
         }
     })).use(timer('metalsmith-layouts'))
+	.use(file_staging)
+	.use(timer('file staging'))
     .use(require('metalsmith-less')())
     .use(timer('metalsmith-less'))
     .use(bower)
