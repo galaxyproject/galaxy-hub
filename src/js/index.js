f9d395bfa7270017934471fe1120a119b81d5fc3
Dannon Baker <dannon.baker@gmail.com>
2018-08-03 15:31:40 -0400

    Add font-awesome dep, bundle, drop hljs from bundle and rely on cdn

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,4 @@
 import "bootstrap";
 import $ from "jquery";
-import * as hljs from "highlight.js";
 import "../style/styles.scss";
-
-window.hljs = hljs;
 window.$ = $;
-console.debug("Galaxy Hub loaded.");
