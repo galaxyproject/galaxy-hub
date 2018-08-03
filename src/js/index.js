46e966dd4d5a35f7dde8e36607f9ff043245f29e
Dannon Baker <dannon.baker@gmail.com>
2018-08-03 14:39:01 -0400

    Strip less, add index.js

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import "bootstrap";
+import $ from "jquery";
+import * as hljs from "highlight.js";
+import "../style/styles.scss";
+
+window.hljs = hljs;
+window.$ = $;
+console.debug("Galaxy Hub loaded.");
