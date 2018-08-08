d17a19206469c0979a0378efb467e3c38781b1fe
Dannon Baker <dannon.baker@gmail.com>
2018-08-08 15:39:15 -0400

    Custom Zotero search interface implementation.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
 import "bootstrap";
 import $ from "jquery";
 import "../style/styles.scss";
+import axios from "axios";
+import _ from "lodash";
 window.$ = $;
+window.axios = axios;
+window._ = _;
