8cbe793c9a2bfc383de66789ef357c4e4c2e2611
Nick <nmapsy@gmail.com>
2021-07-29 16:36:57 -0400

    Add Markdown fixing to JS preprocessor.

--- scripts/mdfixer.mjs
+++ scripts/mdfixer.mjs
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import process from 'process';
+import { fileURLToPath } from 'url';
 import unified from 'unified';
 import unifiedArgs from 'unified-args';
 import remarkParse from 'remark-parse';
@@ -9,6 +10,7 @@ import { Command } from 'commander';
 import keepNewlineBeforeHtml from './keep-newline-before-html.mjs';
 import htmlImgToMd from './html-img-to-md.mjs';
 import fixLinks from './fix-links.mjs';
+import { repr } from '../src/utils.js';
 
 const REMARK_STRINGIFY_OPTIONS = {
   fences:true, rule:'-', listItemIndent:'one', setext:true,
@@ -19,9 +21,14 @@ const program = new Command();
 program
   .arguments('<input>')
   .option('-q, --quiet', 'Output only warnings and errors.')
-  .option('-o, --output [path]', 'Specify output location')
-  .option('-b, --base <path>', 'The root content directory for this input file. Only needed when '
-    +'working on a single input file, not a directory.'
+  .option(
+    '-o, --output [path]',
+    'Specify output location. Give without a path to overwrite the original file(s).'
+  )
+  .option(
+    '-b, --base <path>',
+    'The root content directory for this input file. Only needed when working on a single input '
+    +'file, not a directory.'
   )
   .option('--inspect', 'Output formatted syntax tree.')
   .option('-l, --limit <limit>', 'Only process this many html nodes in the <img> replacer.',
@@ -29,9 +36,14 @@ program
   )
   .option('--debug')
   .action(main);
-program.parse(process.argv);
 
-function main(inputPath, opts) {
+// If the current script is being executed as a command, parse the arguments and run.
+// Otherwise, we're being imported as a module.
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  program.parse(process.argv);
+}
+
+export default function main(inputPath, opts) {
   let base;
   if (opts.base) {
     base = opts.base;
@@ -40,12 +52,13 @@ function main(inputPath, opts) {
   } else if (opts.quiet !== true) {
     console.error(`No base identified. Can't fix relative img src paths unless a --base is given.`);
   }
-  removeExtraArgs(process.argv);
+  // Fix up process.argv so `unified-args` will be happy.
+  process.argv = fixArgv(process.argv, [inputPath], opts);
 
-  // Note: unified-engine is made to make filesystem traversal easy:
-  //       https://github.com/unifiedjs/unified-engine
   // Parse Markdown with remark-parse, parse the frontmatter, modify it with our plugins, then
   // serialize it right back to Markdown with remark-stringify.
+  // Note: unified-engine is made to make filesystem traversal easy:
+  //       https://github.com/unifiedjs/unified-engine
   //TODO: Both htmlImgToMd and fixLinks parse the HTML of each `html` node. If it's a performance
   //      issue, we could cache the parsed `hast` tree in a property on the node. Then maybe a
   //      separate plugin at the end could handle stringifying it back into HTML.
@@ -70,6 +83,20 @@ function main(inputPath, opts) {
   });
 }
 
+// `unified-args` does its own inspection of `process.argv`.
+// We need to make sure those arguments are how it likes it, otherwise it'll throw a fit.
+function fixArgv(currentArgv, positionals, opts) {
+  if (process.argv[1] === fileURLToPath(import.meta.url)) {
+    // If this is being executed directly as a script, all we have to do is remove arguments that
+    // `unified-args` doesn't recognize.
+    return removeExtraArgs(currentArgv.slice());
+  } else {
+    // If this is being used as a module, the current process.argv is irrelevant: it's the arguments
+    // to the calling executable, not this script. We have to construct an artificial argv.
+    return constructArgv(positionals, opts);
+  }
+}
+
 function removeExtraArgs(argv) {
   // Options that take arguments.
   for (let arg of ['-b', '--base', '-l', '--limit']) {
@@ -78,11 +105,36 @@ function removeExtraArgs(argv) {
       argv.splice(i, 2)
     }
   }
-  // Flags
+  // Flags.
   for (let arg of ['--debug']) {
     let i = argv.indexOf(arg);
     if (i >= 0) {
       argv.splice(i, 1);
     }
   }
+  return argv;
+}
+
+function constructArgv(positionals, opts) {
+  let argv = [process.argv[0], process.argv[1]];
+  // Flags.
+  for (let arg of ['quiet', 'inspect']) {
+    if (opts[arg]) {
+      argv.push(`--${arg}`);
+    }
+  }
+  // Positionals.
+  if (positionals.length !== 1) {
+    throw repr`Invalid arguments: Must give one positional; instead found ${positionals}`;
+  }
+  let inputPath = positionals[0];
+  argv.push(inputPath);
+  // Special cases.
+  if (opts.output) {
+    argv.push('--output');
+    if (opts.output !== true) {
+      argv.push(opts.output);
+    }
+  }
+  return argv;
 }
--- src/build/preprocessor.mjs
+++ src/build/preprocessor.mjs
@@ -1,8 +1,10 @@
+#!/usr/bin/env node
 import fs from 'fs';
 import nodePath from 'path';
 import nodeWatch from 'node-watch';
 import { Command } from 'commander';
 import { Partitioner, CONTENT_TYPES } from './partition-content.mjs';
+import mdfixer from '../../scripts/mdfixer.mjs';
 import { repr, PathInfo } from '../utils.js';
 
 // When running `gridsome build` or `develop`, links are sufficient for it to do the right thing
@@ -12,6 +14,9 @@ const PREPROCESS_PLACERS = {md:'link', vue:'copy', insert:'link', resource:'link
 // has been edited, it notices and does some work, but ultimately doesn't recompile the page.
 // Even worse, if a link is ever broken, it crashes.
 const WATCH_PLACERS = {md:'copy', vue:'copy', insert:'copy', resource:'copy'};
+// If we're running mdfixer.mjs on the build directory after partitioning, the Markdown files can't
+// be links. If they are, mdfixer.mjs will overwrite the targets of the links, altering the originals.
+const MDFIX_PREPROCESS_PLACERS = {md:'copy', vue:'copy', insert:'copy', resource:'link'};
 
 // Define command line arguments.
 let program = new Command();
@@ -40,13 +45,14 @@ program
                      'link': Link to the original file from the build directory.
                      'copy': Place a copy of the original file in the build directory.`
   )
+  .option('-F, --no-fix-markdown')
   .option('-v, --verbose', 'Output only warnings and errors.')
   .option('-n, --simulate', 'Do not make any actual changes to the filesystem.')
   .option('--debug', 'Print debug output.')
   .action(main);
 
 for (let contentType of CONTENT_TYPES) {
-  program = program.option(
+  program.option(
     `--${contentType} <name>`,
     repr`Set the method of placing ${contentType} files. Overrides default --placer.`
   );
@@ -68,11 +74,17 @@ function main(command, opts) {
   }
   // Assign placers.
   let placers = {};
+  let defaultPlacers;
   if (command === 'preprocess') {
-    Object.assign(placers, PREPROCESS_PLACERS);
+    if (opts.fixMarkdown) {
+      defaultPlacers = MDFIX_PREPROCESS_PLACERS;
+    } else {
+      defaultPlacers = PREPROCESS_PLACERS;
+    }
   } else if (command === 'watch') {
-    Object.assign(placers, WATCH_PLACERS);
+    defaultPlacers = WATCH_PLACERS;
   }
+  Object.assign(placers, defaultPlacers);
   for (let contentType of CONTENT_TYPES) {
     if (opts[contentType]) {
       placers[contentType] = opts[contentType];
@@ -84,17 +96,48 @@ function main(command, opts) {
   );
   // Execute command.
   if (command === 'watch') {
+    //TODO: If `opts.fixMarkdown`, first walk the build dir to make sure there are no links to
+    //      Markdown files. That'd dangerous, since running `mdfixer` could overwrite the sources.
     function handleEvent(eventType, path) {
       partitioner.handleEvent(eventType, path);
+      if (opts.fixMarkdown) {
+        fixMdOnEvent(partitioner, path, partitioner.verbose, partitioner.simulate);
+      }
     }
     let watcher = nodeWatch(partitioner.contentDir, {recursive:true}, handleEvent);
     //TODO: Wait for a gridsome develop process to appear, then exit once it dies.
   } else if (command === 'preprocess') {
+    console.log('Placing files into build directories..');
     doPrePartitioning(partitioner, opts.clear, partitioner.simulate, partitioner.verbose);
     partitioner.placeDirFiles(partitioner.contentDir, true);
+    if (opts.fixMarkdown) {
+      console.log('Fixing Markdown files..');
+      for (let buildDir of Object.values(partitioner.buildDirs)) {
+        mdfixer(buildDir, {quiet:!partitioner.verbose, output:true});
+      }
+    }
   }
 }
 
+
+function fixMdOnEvent(partitioner, path, verbose, simulate) {
+  let buildPath = partitioner.findBuildPath(path);
+  if (! buildPath) {
+    return;
+  }
+  let buildPathDir;
+  if (new PathInfo(buildPath).type() === 'dir') {
+    buildPathDir = buildPath;
+  } else {
+    buildPathDir = nodePath.dirname(buildPath);
+  }
+  if (verbose) {
+    console.log(repr`Fixing Markdown in ${buildPathDir}`);
+  }
+  if (! simulate) {
+    mdfixer(buildPathDir, {quiet:!partitioner.verbose, output:true});
+  }
+}
 
 function doPrePartitioning(config, clear, simulate, verbose) {
   setupBuildDirs(config.buildDirs, clear, simulate, verbose);
