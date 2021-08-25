453e2936c99dd4d51f047d183537e8afa0ffb629
Nick <nmapsy@gmail.com>
2021-08-25 18:02:13 -0400

    utils.js: Add some set operations and fix placement of JSDoc comments.

--- src/utils.js
+++ src/utils.js
@@ -12,12 +12,12 @@ const remarkHtml = require('remark-html');
  * That's the `module.exports.slugify = slugify` pattern.
  */
 
+/** Template literal tag that converts all embedded values to their literal representations.
+ *  Uses `util.inspect()` for the conversions.
+ *  Alternatively, this can be used as a (single-argument) alias for `util.inspect`.
+ *  Just call it as a regular function with one argument (E.g. `repr(obj)`).
+ */
 function repr(strParts, ...values) {
-  /** Template literal tag that converts all embedded values to their literal representations.
-   *  Uses `util.inspect()` for the conversions.
-   *  Alternatively, this can be used as a (single-argument) alias for `util.inspect`.
-   *  Just call it as a regular function with one argument (E.g. `repr(obj)`).
-   */
   if (strParts.length === undefined && values.length === 0) {
     // Being used as a util.inspect alias.
     return util.inspect(strParts);
@@ -35,6 +35,35 @@ function repr(strParts, ...values) {
 }
 module.exports.repr = repr;
 
+function contains(iterable, element) {
+  return !! (iterable.indexOf(element) > -1);
+}
+module.exports.contains = contains;
+
+// Set operations from https://exploringjs.com/impatient-js/ch_sets.html#missing-set-operations
+
+/** Get the union of two arrays (or any other spreadable iterable).
+ *  The elements are deduplicated by `Set`, so they must work as `Set` keys.
+ */
+function getUnion(list1, list2) {
+  let unionSet = new Set([...list1, ...list2]);
+  return [...unionSet];
+}
+module.exports.getUnion = getUnion;
+
+/** Get the intersection of two iterables.
+ *  The elements are identified by `Set`, so they must work as `Set` keys.
+ */
+function getIntersection(list1, list2) {
+  let set1 = new Set(list1);
+  let set2 = new Set(list2);
+  let interSet = new Set(
+    [...set1].filter(e => set2.has(e))
+  );
+  return [...interSet];
+}
+module.exports.getIntersection = getIntersection;
+
 function slugify(string) {
   return string.toLowerCase().replace(/[^\w\d -]/g, '').replace(/[ -]+/g,'-');
 }
@@ -45,23 +74,23 @@ function splitlines(text) {
 }
 module.exports.splitlines = splitlines;
 
+/** Turn a string containing only a date in ISO 8601 format into a `Date` in the local system's
+ *  timezone.
+ *  @param {string} dateStr A date in ISO 8601 (YYYY-MM-DD) format.
+ */
 function strToDate(dateStr) {
-  /** Turn a string containing only a date in ISO 8601 format into a `Date` in the local system's
-   *  timezone.
-   *  @param {string} dateStr A date in ISO 8601 (YYYY-MM-DD) format.
-   */
   return new Date(dateStr+'T00:00:00');
 }
 module.exports.strToDate = strToDate;
 
+/** Turn a `Date` object into a string showing only the date portion.
+ *  @param {Date} date       A Javascript `Date`. This should be produced using `strToDate()`.
+ *  @param {string} [format='iso'] The style of string representation for the `date`:
+ *    `'iso'`: `"2021-03-12"`
+ *    `'long'`: `"March 3, 2021"`
+ *    `'D MMMM YYYY'`: `"3 March 2021"`
+ */
 function dateToStr(date, format='iso') {
-  /** Turn a `Date` object into a string showing only the date portion.
-   *  @param {Date} date       A Javascript `Date`. This should be produced using `strToDate()`.
-   *  @param {string} [format='iso'] The style of string representation for the `date`:
-   *    `'iso'`: `"2021-03-12"`
-   *    `'long'`: `"March 3, 2021"`
-   *    `'D MMMM YYYY'`: `"3 March 2021"`
-   */
   if (format === 'iso') {
     return date.toISOString().slice(0,10);
   } else if (format === 'long') {
@@ -72,10 +101,10 @@ function dateToStr(date, format='iso') {
 }
 module.exports.dateToStr = dateToStr;
 
+/** Get the difference, in whole days, between two date strings.
+ *  E.g. `dateStrDiff('2021-04-16', '2021-04-14') === 2`
+ */
 function dateStrDiff(date1, date2) {
-  /** Get the difference, in whole days, between two date strings.
-   *  E.g. `dateStrDiff('2021-04-16', '2021-04-14') === 2`
-   */
   let date1date = strToDate(date1);
   let date2date = strToDate(date2);
   return Math.round((date1date-date2date)/1000/60/60/24);
@@ -169,8 +198,8 @@ function titlecase(rawString) {
 }
 module.exports.titlecase = titlecase;
 
+/** Create the same effect as adding a tab to the string, except use spaces. */
 function spaceTab(rawStr, tabWidth=8) {
-  /** Create the same effect as adding a tab to the string, except use spaces. */
   let tabStop = tabWidth*(1+Math.floor(rawStr.length/tabWidth));
   return rawStr.padEnd(tabStop);
 }
@@ -194,15 +223,15 @@ function rmPathPrefix(path, depth, absolute=null) {
 }
 module.exports.rmPathPrefix = rmPathPrefix;
 
+/**
+ * Find all the children of `rootDir`.
+ * Arguments:
+ *   `rootDir` (`String`): An absolute or relative path of a directory.
+ * Returns:
+ *   `files` (`Array`): An array of paths relative to the same directory as the `rootDir`.
+ *     Returns only the paths to files (tested by `isFile()`).
+ */
 function getFilesDeep(rootDir) {
-  /**
-   * Find all the children of `rootDir`.
-   * Arguments:
-   *   `rootDir` (`String`): An absolute or relative path of a directory.
-   * Returns:
-   *   `files` (`Array`): An array of paths relative to the same directory as the `rootDir`.
-   *     Returns only the paths to files (tested by `isFile()`).
-   */
   let files = [];
   let children = fs.readdirSync(rootDir, {withFileTypes: true});
   for (let child of children) {
@@ -284,10 +313,10 @@ class PathInfo {
     //      This will prevent it from updating as the filesystem state changes, though.
     this.path = path;
   }
+  /** Does the path exist?
+   *  Note: This returns `true` for broken symlinks (the link exists, but its targets does not).
+   */
   exists() {
-    /** Does the path exist?
-     *  Note: This returns `true` for broken symlinks (the link exists, but its targets does not).
-     */
     if (fs.existsSync(this.path)) {
       return true;
     } else {
@@ -303,12 +332,12 @@ class PathInfo {
     let pathInfo = new this(path);
     return pathInfo.exists();
   }
+  /** Get the type of filesystem object this path points to.
+   *  If the path does not exist, return `'nonexistent'`.
+   *  If the path is a symbolic link, return the type of its target. If the target is missing,
+   *  return `'brokenlink'`.
+   */
   type() {
-    /** Get the type of filesystem object this path points to.
-     *  If the path does not exist, return `'nonexistent'`.
-     *  If the path is a symbolic link, return the type of its target. If the target is missing,
-     *  return `'brokenlink'`.
-     */
     if (! this.exists()) {
       return 'nonexistent';
     }
