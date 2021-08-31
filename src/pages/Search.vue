9507d45a1b749bcb41370c97b2dec21400bc9164
Nick <nmapsy@gmail.com>
2021-08-31 15:35:19 -0400

    Implement Zotero search. - Also remove unused jQuery dependency.

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -9,11 +9,12 @@
                         <a
                             id="google-tab"
                             class="nav-link active"
-                            href="#google-pane"
                             data-toggle="tab"
+                            data-toggle-group="search-panes"
                             role="tab"
                             aria-controls="google-pane"
                             aria-selected="true"
+                            @click.prevent="switchPane('google', 'search-panes')"
                         >
                             Pan-Galactic Google Search
                         </a>
@@ -21,23 +22,56 @@
                     <li class="nav-item">
                         <a
                             id="zotero-tab"
-                            class="nav-link active"
-                            href="#zotero-pane"
+                            class="nav-link"
                             data-toggle="tab"
+                            data-toggle-group="search-panes"
                             role="tab"
                             aria-controls="zotero-pane"
                             aria-selected="false"
+                            @click.prevent="switchPane('zotero', 'search-panes')"
                         >
                             Galaxy Publication Search
                         </a>
                     </li>
                 </ul>
                 <div class="tab-content">
-                    <div id="google-pane" class="tab-pane active" role="tabpanel" aria-labelledby="home-tab">
+                    <div
+                        id="google-pane"
+                        class="tab-pane active show"
+                        data-toggle-group="search-panes"
+                        role="tabpanel"
+                        aria-labelledby="home-tab"
+                    >
                         <gcse:searchresults-only></gcse:searchresults-only>
                     </div>
-                    <div id="zotero-pane" class="tab-pane" role="tabpanel" aria-labelledby="zotero-tab">
-                        <p>
+                    <div
+                        id="zotero-pane"
+                        class="tab-pane"
+                        data-toggle-group="search-panes"
+                        role="tabpanel"
+                        aria-labelledby="zotero-tab"
+                    >
+                        <p v-if="error">
+                            {{ error }}
+                        </p>
+                        <ul v-else-if="zoteroResults" class="list-group list-group-flush">
+                            <template v-if="zoteroResults.length > 0">
+                                <li v-for="result of zoteroResults" :key="result.data.url" class="list-group-item">
+                                    <h4>
+                                        <a :href="result.data.url">{{ result.data.title }}</a>
+                                    </h4>
+                                    <p class="resultUrl">{{ result.data.url }}</p>
+                                    <p v-if="result.data.abstractNote">
+                                        {{ result.data.abstractNote.slice(0, 450) }}
+                                    </p>
+                                </li>
+                            </template>
+                            <p v-else>
+                                Search finished for <strong>{{ query }}</strong> with no results. Please refine your
+                                query and try again!
+                            </p>
+                        </ul>
+                        <p v-else>
                             Loading Zotero Results
                             <i class="fa fa-spinner fa-spin"></i>
                         </p>
@@ -49,6 +83,24 @@
 </template>
 
 <script>
+import { switchPane } from "~/utils.js";
+import axios from "axios";
+export default {
+    data() {
+        return {
+            query: "",
+            zoteroResults: null,
+            error: null,
+        };
+    },
+    methods: {
+        switchPane,
+    },
+    mounted() {
+        createGoogleSearch();
+        zoteroSearchOnLoad(this);
+    },
+};
 function createGoogleSearch() {
     var cx = "007594916903876912968:w0nrox8rzzy";
     var gcse = document.createElement("script");
@@ -58,11 +110,27 @@ function createGoogleSearch() {
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(gcse, s);
 }
-export default {
-    mounted() {
-        createGoogleSearch();
-    },
-};
+function zoteroSearchOnLoad(instance) {
+    let match = RegExp("[?&]q=([^&]*)").exec(window.location.search);
+    let query = match && decodeURIComponent(match[1].replace(/\+/g, " "));
+    if (query) {
+        instance.query = query;
+    } else {
+        console.error("Could not find a search query in the page url.");
+        return;
+    }
+    let searchInput = document.getElementById("search-input");
+    searchInput.value = query;
+    axios
+        .get(`https://api.zotero.org/groups/1732893/items/top?start=0&limit=25&q=${query}`)
+        .then((response) => {
+            instance.zoteroResults = response.data;
+        })
+        .catch((error) => {
+            console.error("Zotero search failed:", error);
+            instance.error = error;
+        });
+}
 </script>
 
 <page-query>
@@ -79,3 +147,12 @@ query {
   }
 }
 </page-query>
+
+<style scoped>
+a.nav-link {
+    cursor: pointer;
+}
+.resultUrl {
+    color: #093;
+}
+</style>
