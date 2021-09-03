714d8c7e56bdb5edcbac359555cfe16f45466aba
Nick <nmapsy@gmail.com>
2021-09-03 16:45:48 -0400

    Add a real 404 page (fixes #759).

---
+++ src/pages/404.vue
@@ -0,0 +1,26 @@
+<template>
+    <Layout>
+        <h1 class="page-title">{{ $page.main.title }}</h1>
+        <div class="markdown" v-html="$page.main.content" />
+    </Layout>
+</template>
+
+<script>
+export default {
+    metaInfo() {
+        return {
+            title: this.$page.main.title,
+        };
+    },
+}
+</script>
+
+<page-query>
+query {
+  main: insert(path: "/insert:/404/") {
+    id
+    title
+    content
+  }
+}
+</page-query>
