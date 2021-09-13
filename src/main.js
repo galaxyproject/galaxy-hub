f8cd3e115ee45d6f89ab0524466c842ca37afd8c
Dannon Baker <dannon.baker@gmail.com>
2021-09-13 15:54:26 -0400

    Inject bootstrap+jquery into head to avoid build time issues

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+import "~/assets/styles.scss";
+
 import DefaultLayout from "~/layouts/Default.vue";
 import BootstrapVue from "bootstrap-vue";
 
-import "~/assets/styles.scss";
-import "bootstrap";
-
 export default function (Vue, { router, head, isClient }) {
+    head.script.push({
+        src: "//unpkg.com/jquery@3.3.1/dist/jquery.slim.min.js",
+    });
+    head.script.push({
+        src: "//unpkg.com/bootstrap@4.5.0/dist/js/bootstrap.min.js",
+    });
     head.script.push({
         src: "https://sidecar.gitter.im/dist/sidecar.v1.js",
         async: true,
