5ee82f007a4328c9577a0befe2bb5f4532f4ce9c
Dannon Baker <dannon.baker@gmail.com>
2021-09-17 16:25:12 -0400

    Add gitter sidecar test

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -6,4 +6,8 @@ describe("Main Page Tests", () => {
         cy.get("h2 > a[href='/news/']").should("be.visible");
         cy.get("h2 > a[href='/events/']").should("be.visible");
     });
+    it("Opens gitter sidecar and the iframe is injected", () => {
+        cy.get(".gitter-open-chat-button").click();
+        cy.get("iframe[src='https://gitter.im/galaxyproject/Lobby/~embed']").should("be.visible");
+    });
 });
