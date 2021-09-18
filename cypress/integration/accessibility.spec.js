4042a56edce15a00a56a4eafde935dd0470557ed
Dannon Baker <dannon.baker@gmail.com>
2021-09-18 14:11:43 -0400

    Add axe for a11y testing, add basic accessibility test for main page (which does not pass; we should address some of these)

---
+++ cypress/integration/accessibility.spec.js
@@ -0,0 +1,9 @@
+describe("Accessibility Testing", () => {
+    beforeEach(() => {
+        cy.visit("/").get("#app").injectAxe()
+    })
+
+    it("Has no detectable a11y violations on load", () => {
+        cy.checkA11y();
+    });
+});
