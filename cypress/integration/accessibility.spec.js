410077a500fa3d19d92ed48114c9b98898b2c241
Dannon Baker <dannon.baker@gmail.com>
2021-09-20 16:08:03 -0400

    Swap to only critical errors for a11y; test use page as well

--- cypress/integration/accessibility.spec.js
+++ cypress/integration/accessibility.spec.js
@@ -1,9 +1,21 @@
-describe("Accessibility Testing", () => {
-    beforeEach(() => {
-        cy.visit("/").get("#app").injectAxe()
-    })
+const CYPRESS_ACCESSIBILITY_CONFIG = {
+    //includedImpacts: ["serious", "critical"],
+    includedImpacts: ["critical"],
+};
 
+describe("Accessibility Testing", () => {
     it("Has no detectable a11y violations on load", () => {
-        cy.checkA11y("#app");
+        cy.visit("/").get("#app").injectAxe();
+        // Ensure masthead has loaded
+        cy.get("#masthead-logo").should("be.visible");
+        // Only check for #app; ignores twitter and sidecar.
+        cy.checkA11y("#app", CYPRESS_ACCESSIBILITY_CONFIG);
+    });
+    it("Has no detectable a11y violations on load", () => {
+        cy.visit("/use/").get("#app").injectAxe();
+        // Ensure masthead has loaded
+        cy.get("#masthead-logo").should("be.visible");
+        // Only check for #app; ignores twitter and sidecar.
+        cy.checkA11y("#app", CYPRESS_ACCESSIBILITY_CONFIG);
     });
 });
