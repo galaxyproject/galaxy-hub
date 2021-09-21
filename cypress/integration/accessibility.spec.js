cd97fa8f27629c336a68f36c2ec9ae082cc027a7
Dannon Baker <dannon.baker@gmail.com>
2021-09-21 10:22:02 -0400

    Add gcc2022 page to a11y testing

--- cypress/integration/accessibility.spec.js
+++ cypress/integration/accessibility.spec.js
@@ -4,15 +4,22 @@ const CYPRESS_ACCESSIBILITY_CONFIG = {
 };
 
 describe("Accessibility Testing", () => {
-    it("Has no detectable a11y violations on load", () => {
+    it("Homepage has no detectable a11y violations on load", () => {
         cy.visit("/").get("#app").injectAxe();
         // Ensure masthead has loaded
         cy.get("#masthead-logo").should("be.visible");
         // Only check for #app; ignores twitter and sidecar.
         cy.checkA11y("#app", CYPRESS_ACCESSIBILITY_CONFIG);
     });
-    it("Has no detectable a11y violations on load", () => {
+    it("Use page has no detectable a11y violations on load", () => {
         cy.visit("/use/").get("#app").injectAxe();
+        // Ensure masthead has loaded
+        cy.get("#masthead-logo").should("be.visible");
+        // Only check for #app; ignores twitter and sidecar.
+        cy.checkA11y("#app", CYPRESS_ACCESSIBILITY_CONFIG);
+    });
+    it("GCC2022 page has no detectable a11y violations on load", () => {
+        cy.visit("/events/gcc2022/").get("#app").injectAxe();
         // Ensure masthead has loaded
         cy.get("#masthead-logo").should("be.visible");
         // Only check for #app; ignores twitter and sidecar.
