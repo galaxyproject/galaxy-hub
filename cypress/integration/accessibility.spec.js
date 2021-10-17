32ea3218177dedfeccdaaf59c4b931316416e215
Dannon Baker <dannon.baker@gmail.com>
2021-10-17 12:34:32 -0400

    Force cypress to wait for animation transition prior to analyzing accessibility

--- cypress/integration/accessibility.spec.js
+++ cypress/integration/accessibility.spec.js
@@ -12,11 +12,14 @@ describe("Accessibility Testing", () => {
         // Only check for #app; ignores twitter and sidecar.
         cy.checkA11y("#app", CYPRESS_ACCESSIBILITY_CONFIG);
     });
-    it("Use page has no detectable a11y violations on load", () => {
+    it.only("Use page has no detectable a11y violations on load", () => {
         cy.visit("/use/").get("#app").injectAxe();
         // Ensure masthead has loaded
         cy.get("#masthead-logo").should("be.visible");
-        // Only check for #app; ignores twitter and sidecar.
+        // We really shouldn't be using cy.wait, but the problem here is that
+        // the table contents fade in and we have to wait for the animation to
+        // complete prior to checking contrast.
+        cy.wait(500);
         cy.checkA11y("#app", CYPRESS_ACCESSIBILITY_CONFIG);
     });
     it("GCC2022 page has no detectable a11y violations on load", () => {
