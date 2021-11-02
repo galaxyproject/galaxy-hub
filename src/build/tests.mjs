1ac86d82ab092b9940a9db53ce530d9f26533ead
Nick <nmapsy@gmail.com>
2021-11-02 16:35:37 -0400

    Add custom handler to compile Markdown links without escaping the urls.

--- src/build/tests.mjs
+++ src/build/tests.mjs
@@ -1,3 +1,5 @@
+// fix-links.mjs
+
 import { fixHyperLink } from "./fix-links.mjs";
 
 const WHITELISTED_HREFS = [
@@ -41,5 +43,45 @@ test("Fix relative urls", () => {
     for (let [rawHref, expectedValue] of Object.entries(RELATIVE_HREFS)) {
         let result = fixHyperLink(rawHref);
         expect(result).toBe(expectedValue);
+    }
+});
+
+
+// unescape-links.mjs
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkStringify from 'remark-stringify';
+import unescapeLink from './unescape-links.mjs';
+
+const UNALTERED_LINKS = [
+    '[Google](https://google.com/search?channel=fs&q=query)',
+    '[](https://google.com/search?channel=fs&q=query)',
+    '[ ](https://google.com/search?channel=fs&q=query)',
+    '[Search](https://galaxyproject.org/search/?q=query#gsc.tab=0&gsc.q=query&gsc.page=1)',
+    '[Google *search*](https://google.com/search?channel=fs&q=query "This is a title"))',
+    `[Google](https://google.com/search?channel=fs&q=query 'A "title"')`,
+    '[![Google](https://google.com/logo.jpg)](https://google.com/search?channel=fs&q=query)',
+    '[\nGoogle *search*\n![alt](img.jpg)\n](https://google.com/search?channel=fs&q=query "Title text")',
+    '<https://google.com/search?channel=fs&q=query>',
+];
+const ALTERED_LINKS = {
+    '[Google *search*](https://google.com/search?channel=fs&q=query ""))':
+        '[Google *search*](https://google.com/search?channel=fs&q=query))',
+    '[ Google ]( https://google.com/search?channel=fs&q=query )':
+        '[ Google ](https://google.com/search?channel=fs&q=query)',
+}
+test("Unescape links", () => {
+    const processor = unified()
+        .use(remarkParse)
+        .use(remarkStringify, {resourceLink: false, handlers: {link: unescapeLink}});
+    let testCases = {};
+    Object.assign(testCases, ALTERED_LINKS);
+    for (let input of UNALTERED_LINKS) {
+        testCases[input] = input;
+    }
+    for (let [input, expectedValue] of Object.entries(testCases)) {
+        let tree = processor.parse(input);
+        let result = processor.stringify(tree);
+        expect(result).toBe(expectedValue+'\n');
     }
 });
---
+++ src/build/unescape-links.mjs
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+import { unified } from 'unified';
+import remarkStringify from 'remark-stringify';
+
+const QUOTE_MAP = {'"':"'", "'":'"'};
+const remarkCompiler = unified().use(remarkStringify);
+
+export default function unescapeLink(node, parent, context) {
+  /** Custom handler for Link mdast nodes. This prevents special characters like `&` and `_` from being escaped in
+   * link urls.
+   */
+  /* We can't just compile the node with the default handler, then use the
+   * node's position data to replace the url in the resulting string.
+   * Firstly, the input node's position data refers to the position in the originally parsed file,
+   * but different plugins may have altered it between parsing the file and receiving it here.
+   * Second, there's no position data on where the individual elements of the link are (the url, the title, etc).
+   * So instead we stringify it manually, except the child elements which we use remark to stringify separately.
+   */
+  if (! context.options.resourceLink && isAutolink(node)) {
+    return `<${node.url}>`;
+  }
+  let childrenStrRaw;
+  if (node.children.length === 0) {
+    childrenStrRaw = '';
+  } else if (node.children.length === 1) {
+    childrenStrRaw = remarkCompiler.stringify(node.children[0]);
+  } else {
+    // Make a copy of the original node, but replace the url with a simple dummy one of known length ("/") and remove
+    // any title.
+    let dummyNode = {};
+    Object.assign(dummyNode, node);
+    dummyNode.url = '/';
+    dummyNode.title = null;
+    let dummyStr = remarkCompiler.stringify(dummyNode);
+    childrenStrRaw = dummyStr.slice(1,dummyStr.length-4);
+  }
+  let childrenStr = '';
+  if (childrenStrRaw) {
+    // Remove the trailing newline inserted by remark.
+    childrenStr = childrenStrRaw.slice(0, childrenStrRaw.length-1);
+  }
+  let titleStr = '';
+  if (node.title !== null) {
+    // Quote the title with the user-preferred quote character, if possible.
+    let quote = context.options.quote || '"';
+    if (node.title.includes(quote)) {
+      let otherQuote = QUOTE_MAP[quote];
+      titleStr = ` ${otherQuote}${node.title}${otherQuote}`;
+    } else {
+      titleStr = ` ${quote}${node.title}${quote}`;
+    }
+  }
+  //TODO: A url shouldn't ever have parentheses in it, but double-check this is impossible.
+  return `[${childrenStr}](${node.url}${titleStr})`;
+}
+
+
+function isAutolink(node) {
+  if (! (node.children.length === 1 && node.children[0].type === 'text')) {
+    return false;
+  }
+  let text = node.children[0].value;
+  return text === node.url;
+}
