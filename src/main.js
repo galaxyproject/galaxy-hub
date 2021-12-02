2677322b937bfbc2e066a523d97dc50df6ab2132
Dannon Baker <dannon.baker@gmail.com>
2021-12-02 15:37:30 -0500

    Register 4 components from sars-cov-2 site for use in pages -- jupyter, observable, flatshield, deferredIframe

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import "~/assets/styles.scss";
 
 import DefaultLayout from "~/layouts/Default.vue";
 import LinkBox from "~/components/LinkBox.vue";
+import FlatShield from "~/components/FlatShield.vue";
+import DeferredIframe from "~/components/deferredIframe.vue";
+import JupyterNotebook from "~/components/JupyterNotebook.vue";
+import ObservableNotebook from "~/components/ObservableNotebook.vue";
+
 import BootstrapVue from "bootstrap-vue";
 
 //eslint-disable-next-line no-unused-vars
@@ -12,6 +17,11 @@ export default function (Vue, { router, head, isClient }) {
     // Set default layout as a global component
     Vue.component("Layout", DefaultLayout);
     Vue.component("LinkBox", LinkBox);
+    Vue.component("FlatShield", FlatShield);
+    Vue.component("DeferredIframe", DeferredIframe);
+    Vue.component("JupyterNotebook", JupyterNotebook);
+    Vue.component("ObservableNotebook", ObservableNotebook);
+
     Vue.use(BootstrapVue);
     Vue.config.ignoredElements = ["gcse:search", "gcse:searchresults-only"];
 }
