e122b516941a5e740825172079f3774fad9c9120
Beatriz <beatrizserrano.galaxy@gmail.com>
2022-01-09 16:27:18 +0100

    attempt to create a new component

--- src/components/HomeProfile.vue
+++ src/components/HomeProfile.vue
@@ -1,29 +1,19 @@
 <template>
-    <div :class="['pseudo-card', `col-sm-${width}`]">
+    <b-button :href="link" variant="outline-primary" class="m-1 border" style="max-width: 19%">
         <h2>
-            <g-link :to="link"
-                ><span :class="`icon ${icon}`"></span><b>{{ title }}</b></g-link
-            >
+            Galaxy for <b>{{ title }}</b>
         </h2>
-        <ItemListBrief v-for="(item, i) in items" :key="item.id || i" :item="item" />
-        <div class="markdown content" v-if="content" v-html="content" />
-    </div>
+        <b-img class="profile-button-img my-3" :src="img" fluid :alt="alt"></b-img>
+    </b-button>
 </template>
 
 <script>
-import ItemListBrief from "@/components/ItemListBrief";
-
 export default {
-    components: {
-        ItemListBrief,
-    },
     props: {
         title: { type: String, required: false, default: "" },
         link: { type: String, required: false, default: "" },
-        icon: { type: String, required: false, default: "" },
-        width: { type: Number, required: false, default: 4 },
-        content: { type: String, required: false, default: "" },
-        items: { type: Array, required: false, default: () => [] },
+        img: { type: String, required: false, default: "" },
+        alt: { type: String, required: false, default: "" },
     },
 };
 </script>
@@ -31,18 +21,7 @@ export default {
 <style lang="scss" scoped>
 @import "~/assets/styles.scss";
 
-.pseudo-card {
-    background-color: $gray-200;
-    border: 4px solid white;
-    border-radius: 8px;
-}
-h2 .icon {
-    margin-right: 0.7em;
-}
-.content.markdown::v-deep p {
-    font-size: 80%;
-}
-.content.markdown::v-deep a {
-    font-size: 120%;
+.profile-button-img {
+    max-height: 5rem;
 }
 </style>
