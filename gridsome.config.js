6ca050cabe4625abe1ffff9038f7a7f78a08977a
Dannon Baker <dannon.baker@gmail.com>
2022-03-09 08:47:08 -0500

    Minor cleanup

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,6 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 const nodePath = require("path");
-const fs = require("fs");
 const jiti = require("jiti")(__filename);
 const remarkToc = jiti("remark-toc").default;
 const tocRemodel = jiti("./src/build/toc-remodel.mjs").default;
@@ -94,7 +93,7 @@ function getPlugin(plugins, typeName) {
 function makeFilenamePath(prefix, node) {
     // Note: `node.fileInfo` is not available from nodes made by `vue-remark`. This is fine as long as
     // this is only used for collections sourced by `source-filesystem` (e.g. `Insert`s).
-    let directory = rmPathPrefix(node.fileInfo.directory, CONTENT_DIR_DEPTH, (absolute = false));
+    let directory = rmPathPrefix(node.fileInfo.directory, CONTENT_DIR_DEPTH, false);
     let path;
     if (directory === "") {
         path = node.fileInfo.name;
