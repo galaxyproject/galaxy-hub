ab9e3b3180991cad6601da04279bd97b9eff1184
Beatriz <beatrizserrano.galaxy@gmail.com>
2022-03-18 19:11:24 +0100

    format

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -17,7 +17,9 @@
                     <b-dropdown-item to="/community/">The Galaxy Community</b-dropdown-item>
                     <b-dropdown-item to="/get-started/">Get started</b-dropdown-item>
                     <b-dropdown-item to="/community/contributing/">How to contribute</b-dropdown-item>
-                    <b-dropdown-item href="https://galaxy-mentor-network.netlify.app/">Galaxy Mentor Network</b-dropdown-item>
+                    <b-dropdown-item href="https://galaxy-mentor-network.netlify.app/"
+                        >Galaxy Mentor Network</b-dropdown-item
+                    >
                     <b-dropdown-item to="/blog/">Blog</b-dropdown-item>
                     <b-dropdown-item to="/community/governance/">Governance</b-dropdown-item>
                     <b-dropdown-item to="/community/coc/">Code of Conduct</b-dropdown-item>
