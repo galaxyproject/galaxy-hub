a5c42219666b8496245427d456a7651551487f77
Nick <nmapsy@gmail.com>
2022-05-09 17:11:15 -0400

    Move Gitter chat embed from index.html to its own component.

---
+++ src/components/Gitter.vue
@@ -0,0 +1,14 @@
+<script>
+// This is in a component instead of a function so that static pages can use it by embedding it in their Markdown.
+export default {
+    mounted() {
+        ((window.gitter = {}).chat = {}).options = { room: "galaxyproject/Lobby" };
+        let head = document.querySelector("html > head");
+        let script = document.createElement("script");
+        script.async = true;
+        script.defer = true;
+        script.src = "https://sidecar.gitter.im/dist/sidecar.v1.js";
+        head.appendChild(script);
+    },
+}
+</script>
