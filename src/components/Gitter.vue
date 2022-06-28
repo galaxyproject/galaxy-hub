806fd15f110f7213045e17b3f7919ef8e9ca3717
Nick <nmapsy@gmail.com>
2022-06-28 19:59:39 -0400

    Allow specifying a Gitter room.

--- src/components/Gitter.vue
+++ src/components/Gitter.vue
@@ -4,8 +4,11 @@
 
 <script>
 // This is in a component instead of a function so that static pages can use it by embedding it in their Markdown.
-function addGitter(window) {
-    ((window.gitter = {}).chat = {}).options = { room: "galaxyproject/Lobby" };
+import { getSingleKey } from "~/lib/utils.js";
+import CONFIG from "~/../config.json";
+const ROOT_SUBSITE = getSingleKey(CONFIG.subsites.hierarchy);
+function addGitter(window, room) {
+    ((window.gitter = {}).chat = {}).options = { room: room };
     let body = document.querySelector("html > body");
     let script = document.createElement("script");
     script.async = true;
@@ -14,9 +17,13 @@ function addGitter(window) {
     body.appendChild(script);
 }
 export default {
+    props: {
+        room: { type: String, required: false, default: "" },
+    },
     mounted() {
         if (!window.gitter) {
-            addGitter(window);
+            let room = this.room || CONFIG.subsites.metadata[ROOT_SUBSITE]?.gitter;
+            addGitter(window, room);
         }
     },
 };
