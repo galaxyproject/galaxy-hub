b7f9eae70e7c2cc8363aa18fe49b282bd73ea5dc
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2022-09-12 17:09:46 +0200

    add news events and twitter sections

--- src/pages/projects/esg/Index.vue
+++ src/pages/projects/esg/Index.vue
@@ -1,6 +1,29 @@
 <template>
     <Layout>
-        <p v-html="$page.main.content"></p>
+        <p v-html="$page.main.content" class="mb-4"></p>
+
+        <div class="news-and-events mb-4">
+            <div class="card shadow-sm bg-light rounded border-0">
+                <h2 class="card-header mt-0">News</h2>
+                <div class="card-body p-2">
+
+                </div>
+            </div>
+
+            <div class="card shadow-sm bg-light rounded border-0">
+                <h2 class="card-header mt-0">Events</h2>
+                <div class="card-body p-2">
+                    
+                </div>
+            </div>
+
+            <div class="card shadow-sm bg-light rounded border-0">
+                <h2 class="card-header mt-0">Twitter</h2>
+                <div class="card-body p-2">
+                    <Twitter link="https://twitter.com/galaxyproject" height="400" width="100%"/>
+                </div>
+            </div>
+        </div>
 
         <Partners title="Project Partners" pathPrefix="/projects/esg/partner-logos/" :partners="$page.dataset.partners"/>
     </Layout>
@@ -9,11 +32,13 @@
 <script>
 import Layout from "~/layouts/Default.vue";
 import Partners from "~/components/Partners.vue";
+import Twitter from "~/components/Twitter.vue";
 
 export default {
     components: {
         Layout,
         Partners,
+        Twitter,
     },
     metaInfo() {
         return {
@@ -44,5 +69,61 @@ query {
             website
         }
     }
+
+    news: allArticle(
+        limit: 5, filter: {category: {eq: "news" }, subsites: {contains: ["esg"]}, draft: {ne: true}}
+    ) {
+        totalCount
+        edges {
+            node {
+                ...articleFields
+            }
+        }
+    }
+
+    events: allArticle(
+        limit: 5, sortBy: "date", order: ASC,
+        filter: {
+            category: {eq: "events"}, subsites: {contains: ["esg"]}, has_date: {eq: true}, days_ago: {lte: 0},
+            draft: {ne: true}
+        }
+    ) {
+        totalCount
+        edges {
+            node {
+                ...articleFields
+                date (format: "MMM D")
+                end  (format: "MMM D")
+            }
+        }
+    }
+}
+
+fragment articleFields on Article {
+    id
+    title
+    tease
+    external_url
+    path
 }
 </page-query>
+
+<style lang="scss" scoped>
+.news-and-events {
+    display: flex;
+    flex-direction: row;
+    align-items: stretch;
+    gap: 2rem;
+
+    // makes sure the twitter component doesn't expand this element on load
+    min-height: 30rem;
+
+    & > * {
+        width: 100%;
+    }
+
+    @media screen and (max-width: 1200px) {
+        flex-direction: column;
+    }
+}
+</style>
