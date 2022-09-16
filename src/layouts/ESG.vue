ecae1cb8e159115702369ffa6155128b194a19cd
Laila Los <44241786+ElectronicBlueberry@users.noreply.github.com>
2022-09-16 13:40:15 +0200

    add solo nav

--- src/layouts/ESG.vue
+++ src/layouts/ESG.vue
@@ -1,5 +1,32 @@
 <template>
     <div class="layout">
+        <b-navbar toggleable="md" type="dark" variant="dark">
+            <div class="container">
+                <b-navbar-brand href="/projects/esg?solo=true">
+                    <img
+                        src="/images/projects/esg/eosc_euro_science_gateway_white.svg"
+                        alt="eosc Euro Science Gateway"
+                        height="30"
+                    />
+                </b-navbar-brand>
+
+                <b-navbar-toggle target="nav-collapse" />
+
+                <b-collapse class="ml-auto" id="nav-collapse" is-nav>
+                    <b-navbar-nav>
+                        <li class="nav-item" :class="{active: activeUrl === 'esg'}">
+                            <a class="nav-link" href="/projects/esg?solo=true"> Home </a>
+                        </li>
+                        <li class="nav-item" :class="{active: activeUrl === 'news'}">
+                            <a class="nav-link" href="/projects/esg/news?solo=true"> News </a>
+                        </li>
+                        <li class="nav-item" :class="{active: activeUrl === 'events'}">
+                            <a class="nav-link" href="/projects/esg/events?solo=true"> Events </a>
+                        </li>
+                    </b-navbar-nav>
+                </b-collapse>
+            </div>
+        </b-navbar>
         <main id="maincontainer" class="container">
             <slot />
         </main>
@@ -8,6 +35,28 @@
 
 <script>
 export default {
+    computed: {
+        activeUrl() {
+            const path = this.$route.path;
 
-}
+            if (path.endsWith("esg")) {
+                return "esg";
+            }
+
+            if (path.endsWith("news")) {
+                return "news";
+            }
+
+            if (path.endsWith("events")) {
+                return "events";
+            }
+        },
+    },
+};
 </script>
+
+<style lang="scss" scoped>
+#nav-collapse {
+    flex-grow: unset;
+}
+</style>
