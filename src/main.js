2fbdb71602c0e3da36b08bb2652c487df37b048b
Nick <git@mailship.org>
2022-09-22 19:00:06 -0400

    Add vue-tweet-embed package, enable in Markdown by default.

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import DefaultLayout from "~/layouts/Default.vue";
 import LinkBox from "~/components/LinkBox.vue";
 import VegaEmbed from "~/components/VegaEmbed.vue";
 import MarkdownEmbed from "~/components/MarkdownEmbed.vue";
+import { Tweet, Timeline } from "vue-tweet-embed";
 
 import BootstrapVue from "bootstrap-vue";
 
@@ -17,6 +18,8 @@ export default function (Vue, { router, head, isClient }) {
     Vue.component("LinkBox", LinkBox);
     Vue.component("VegaEmbed", VegaEmbed);
     Vue.component("MarkdownEmbed", MarkdownEmbed);
+    Vue.component("Tweet", Tweet);
+    Vue.component("Timeline", Timeline);
 
     Vue.use(BootstrapVue);
     Vue.config.ignoredElements = ["gcse:search", "gcse:searchresults-only"];
