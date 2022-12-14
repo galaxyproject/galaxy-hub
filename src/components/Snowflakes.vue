9d45a541a10b6090f26356cc4d1e7066943d1dd0
Dannon Baker <dannon.baker@gmail.com>
2022-12-14 09:48:06 -0500

    Refactor snowflakes to component, add to welcome.  Add duration (default to 60 seconds, after which they go away)

---
+++ src/components/Snowflakes.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <!--
+            snowflake container -- if desired we could scope this to a particualr or use a slot ehre size instead of using the entire body
+        -->
+    </div>
+</template>
+
+<script>
+import Snowflakes from "magic-snowflakes";
+
+export default {
+    name: "Snowflakes",
+    props: {
+        color: { type: String, required: false, default: "#5ECDEF" },
+        count: { type: Number, required: false, default: 100 },
+        speed: { type: Number, required: false, default: 1 },
+        wind: { type: Boolean, required: false, default: true },
+        duration: { type: Number, required: false, default: 60 },
+        minSize: { type: Number, required: false, default: 15 },
+        maxSize: { type: Number, required: false, default: 35 },
+    },
+    mounted() {
+        const snow = new Snowflakes({
+            ...this.$props,
+        });
+        if (this.duration) {
+            setTimeout(() => {
+                snow.destroy();
+            }, this.duration * 1000);
+        }
+    },
+};
+</script>
