d2ee0af96dc011a34cd128efa878981f4978d584
Dannon Baker <dannon.baker@gmail.com>
2023-04-04 13:19:55 -0400

    Include carousel container in component for easier consistent reuse without styling.  Could be argued it should just inherit parent container, but that makes it potentially significantly more aggravating to use from markdown content.

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,42 +1,44 @@
 <template>
-    <b-carousel
-        id="carousel-1"
-        v-model="slide"
-        :interval="4000"
-        controls
-        indicators
-        background="#ababab"
-        img-width="650"
-        img-height="450"
-        style="text-shadow: 1px 1px 2px #333"
-        @sliding-start="onSlideStart"
-        @sliding-end="onSlideEnd"
-    >
-        <a href="https://galaxyproject.org/events/gcc2023/abstracts/" target="_blank">
-            <b-carousel-slide
-                img-src="/images/events/gcc2023/gcc2023-call-for-abstracts.png"
-                img-alt="The Galaxy Community Conference is an annual gathering that brings together scientists, software developers, and trainers of Galaxy from a variety of disciplines to share their experiences, knowledge, and skills."
-            ></b-carousel-slide>
-        </a>
-        <a href="https://galaxyproject.org/events/2023-admin-training/" target="_blank">
-            <b-carousel-slide
-                img-src="/images/events/2023-04-gat/gat23-logo-square.png"
-                img-alt="The Galaxy Admin Training is a free, in-person intensive training workshop for prospective and experienced Galaxy server administrators from April 17 to April 21, 2023 in Ghent, Belgium"
-            ></b-carousel-slide>
-        </a>
-        <a href="https://gxy.io/smorgasbord3" target="_blank">
-            <b-carousel-slide
-                img-src="/images/events/2023-05-smorgasbord3/smorgasbord3-square.png"
-                img-alt="The Galaxy Training Network Smörgåsbord 3 is a free global online Galaxy training event from May 22 to May 26, 2023"
-            ></b-carousel-slide>
-        </a>
-        <a href="https://jxtxfoundation.org" target="_blank">
-            <b-carousel-slide
-                img-src="/images/jxtx/jxtx-foundation.webp"
-                img-alt="Donate to the James P. Taylor Foundation for Open Science"
-            ></b-carousel-slide>
-        </a>
-    </b-carousel>
+    <div class="carousel-container">
+        <b-carousel
+            id="carousel-1"
+            v-model="slide"
+            :interval="4000"
+            controls
+            indicators
+            background="#ababab"
+            img-width="650"
+            img-height="450"
+            style="text-shadow: 1px 1px 2px #333"
+            @sliding-start="onSlideStart"
+            @sliding-end="onSlideEnd"
+        >
+            <a href="https://galaxyproject.org/events/gcc2023/abstracts/" target="_blank">
+                <b-carousel-slide
+                    img-src="/images/events/gcc2023/gcc2023-call-for-abstracts.png"
+                    img-alt="The Galaxy Community Conference is an annual gathering that brings together scientists, software developers, and trainers of Galaxy from a variety of disciplines to share their experiences, knowledge, and skills."
+                ></b-carousel-slide>
+            </a>
+            <a href="https://galaxyproject.org/events/2023-admin-training/" target="_blank">
+                <b-carousel-slide
+                    img-src="/images/events/2023-04-gat/gat23-logo-square.png"
+                    img-alt="The Galaxy Admin Training is a free, in-person intensive training workshop for prospective and experienced Galaxy server administrators from April 17 to April 21, 2023 in Ghent, Belgium"
+                ></b-carousel-slide>
+            </a>
+            <a href="https://gxy.io/smorgasbord3" target="_blank">
+                <b-carousel-slide
+                    img-src="/images/events/2023-05-smorgasbord3/smorgasbord3-square.png"
+                    img-alt="The Galaxy Training Network Smörgåsbord 3 is a free global online Galaxy training event from May 22 to May 26, 2023"
+                ></b-carousel-slide>
+            </a>
+            <a href="https://jxtxfoundation.org" target="_blank">
+                <b-carousel-slide
+                    img-src="/images/jxtx/jxtx-foundation.webp"
+                    img-alt="Donate to the James P. Taylor Foundation for Open Science"
+                ></b-carousel-slide>
+            </a>
+        </b-carousel>
+    </div>
 </template>
 
 <script>
@@ -58,3 +60,10 @@ export default {
     },
 };
 </script>
+<style lang="scss" scoped>
+.carousel-container {
+    padding-top: 2rem;
+    max-width: 650px;
+    margin: 0 auto;
+}
+</style>
