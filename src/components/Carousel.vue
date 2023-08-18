601a62e59625cb1fc3d129f49775f6cbbfe3d9f2
Natalie Whitaker <129767521+natwhitaker@users.noreply.github.com>
2023-08-18 09:53:24 -0400

    Update Carousel.vue with New Paper

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -15,6 +15,12 @@
             It's important that the images here are 13x9 aspect ratio,
             otherwise the page will jump around when the carousel changes.
             -->
+            <a href="https://www.biorxiv.org/content/10.1101/2023.06.28.546576v1.full" target="_blank">
+                <b-carousel-slide
+                    img-src="/images/Paper_Alert_August2023.jpg"
+                    img-alt="New paper in preprint titled Scalable, accessible, and reproducible reference genome assembly and evaluation in Galaxy by Larivière et al. 2023. Click to read!"
+                ></b-carousel-slide>
+            </a>
             <a href="https://galaxyproject.org/events/gcc2023/register/" target="_blank">
                 <b-carousel-slide
                     img-src="/images/events/gcc2023/GCC2023_hub.jpeg"
