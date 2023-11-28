97531ac02c8fde5c7d956c6ec896b1bbcb0912d7
Tyler Collins <tcollins@DESKTOP-0MLKLS8.localdomain>
2023-11-28 12:42:50 -0500

    eslint format fixes

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -21,7 +21,10 @@
                     img-alt="New paper in preprint titled *Scalable, accessible, and reproducible reference genome assembly and evaluation in Galaxy* by Larivière et al. 2023. Click to read!"
                 ></b-carousel-slide>
             </a>
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSecmWqADPLOCWvueILRirV8ClSTsDaP-lDE8m2j6f6pG24NGw/viewform?usp=sf_link" target="_blank">
+            <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLSecmWqADPLOCWvueILRirV8ClSTsDaP-lDE8m2j6f6pG24NGw/viewform?usp=sf_link"
+                target="_blank"
+            >
                 <b-carousel-slide
                     img-src="/images/events/gcc2024/training-survey.jpeg"
                     img-alt="Please help us by providing feedback about what content that you would like to see in the training sessions in GCC2024. Click to fill out the survey!"
