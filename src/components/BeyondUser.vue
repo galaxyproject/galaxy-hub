7a8ab26f52a3dbee59f129fea69c8e92d883cb90
hujambo-dunia <michelle.savage.1@gmail.com>
2023-12-20 10:37:00 -0500

    Updated content section titled: Go Beyond

--- src/components/BeyondUser.vue
+++ src/components/BeyondUser.vue
@@ -5,24 +5,24 @@
         </div>
         <div class="row">
             <div class="col-md-4 mb-3">
-                <a href="/admin/" class="card card-btn bu-card bu-card-bg1 bu-card-bg1-btn text-white">
-                    <div class="card-body">
-                        <h3 class="card-title">Prefer a custom instance?</h3>
-                        <p class="card-text">Galaxy can be installed locally, fine-tuned with a tailored toolset and no
+                <a href="/admin/" class="card card-home bg1 btn">
+                    <div class="text-white">
+                        <h3 class="title">Prefer a custom instance?</h3>
+                        <p class="text">Galaxy can be installed locally, fine-tuned with a tailored toolset and no
                             usage quotas.</p>
                     </div>
                 </a>
             </div>
 
             <div class="col-md-8 mb-3">
-                <div class="card bu-card bu-card-bg1 text-white">
-                    <div class="card-body">
-                        <h3 class="card-title">Need more tools?</h3>
-                        <p class="card-text">The Galaxy ToolShed is a repository of thousands tools that have been
+                <div class="card card-home bg1">
+                    <div class="text-white">
+                        <h3 class="title">Need more tools?</h3>
+                        <p class="text">The Galaxy ToolShed is a repository of thousands tools that have been
                             integrated with Galaxy, and you can add your own.</p>
                         <div class="float-right" style="position: absolute; bottom: 0; right: 1rem;">
-                          <a href="/tools/" class="btn btn-outline-light mr-3">Add tools</a>
-                          <a href="/toolshed/" class="btn btn-info">Browse ToolShed</a>
+                          <a href="/tools/" class="btn btn-lg btn-outline-light mr-3">Add tools</a>
+                          <a href="/toolshed/" class="btn btn-lg btn-info">Browse ToolShed</a>
                         </div>
                     </div>
                 </div>
@@ -30,21 +30,21 @@
         </div>
 
         <div class="row">
-            <div class="col-md-8 mb-3">
-                <a href="/community/" class="card card-btn bu-card bu-card-bg2 text-white">
-                    <div class="card-body">
-                        <h3 class="card-title">Galaxy is a world-wide community.</h3>
-                        <p class="card-text">There are over 500,000 registered Galaxy users from all over the world. Join
+            <div class="col-md-7 mb-3">
+                <a href="/community/" class="card card-home bg2">
+                    <div class="text-dark">
+                        <h3 class="title">Galaxy is a world-wide community.</h3>
+                        <p class="text">There are over 500,000 registered Galaxy users from all over the world. Join
                             this lively community to get help, contribute, and learn.</p>
                         </div>
                 </a>
             </div>
 
-            <div class="col-md-4 mb-3">
-                <div class="card bu-card bu-card-bg3">
-                    <div class="card-body">
-                        <h3 class="card-title">Use Galaxy servers are global!</h3>
-                        <p class="card-text">There are over a hundred public and free Galaxy servers available.
+            <div class="col-md-5 mb-3">
+                <div class="card card-home bg3 img">
+                    <div class="text-dark">
+                        <h3 class="title">Use Galaxy servers are global!</h3>
+                        <p class="text">There are over a hundred public and free Galaxy servers available.<br>
                           <a href="/use/">Find yours.</a>
                         </p>
                     </div>
@@ -67,42 +67,60 @@ export default {
   font-weight: 700;
   color: white;
 }
-.card-title {
-  font-size: 1.5rem;
-  font-weight: 700;
-}
 
-.card-btn, .card-btn:hover {
+.card-home {
+  text-align: left;
+  padding: 20px;
   text-decoration: none !important;
-}
-
-.card-text {
-  font-size: 1.2rem;
-  padding-left: 0;
-}
-
-.bu-card {
   min-height: 15rem;
   border-radius: 20px;
-}
+  &:hover {
+    text-decoration: none !important;
+  }
 
-.bu-card-bg1 {
-  background-color: #0E1427;
-}
+  &.bg1 {
+    background-color: #0E1427;
+    &.btn:hover {
+      background-color: #1B264B;
+    }
+  }
+  
+  &.bg2 {
+    background-color: #00A7E1;
+    &:hover {
+      background-color: #00B8F5;
+    }
+  }
 
-.bu-card-bg1-btn:hover {
-  background-color: #1B264B;
-}
+  &.bg3 {
+    background-color: #76FFFF;
+    position: relative;
 
-.bu-card-bg2 {
-  background-color: #00A7E1;
-}
+    &::before, &:before {
+      content: '';
+      pointer-events: none;
+      display: block;
+      width: 90%;
+      height: 100%;
+      position: absolute;
+      background-image: url('/images/icons/worldMap.svg');
+      opacity: 0.05;
+      background-size: cover;
+      background-position: 0 -200%;
+    }
+  }
 
-.bu-card-bg2:hover {
-  background-color: #00B8F5;
-}
+  .title {
+    font-size: 1.5rem;
+    font-weight: 700;
+    margin-bottom: 0.25rem;
+  }
 
-.bu-card-bg3 {
-  background-color: #76FFFF;
+  .text {
+    font-size: 1.5rem;
+    font-weight: 200;
+    padding-left: 0;
+    line-height: 1.6;
+  }
 }
 </style>
