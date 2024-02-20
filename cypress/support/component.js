2b10b198f4bba95cdd23fb8f2fc134687234e046
Enis Afgan <afgane@gmail.com>
2024-02-20 17:18:34 +0000

    Remove test cypress exception

--- cypress/support/component.js
+++
@@ -1,6 +0,0 @@
-Cypress.on("uncaught:exception", (err, runnable) => {
-    // Dont't fail a test if the error message contains the included text
-    // https://docs.cypress.io/api/cypress-api/catalog-of-events#Uncaught-Exceptions
-    // https://github.com/galaxyproject/galaxy-hub/actions/runs/7921386714/job/21626571641?pr=2388#step:10:92
-    return false;
-});
