1b486426b5dc82950beacce319321ff2b9d49366
Bjoern Gruening <bjoern.gruening@gmail.com>
2026-01-24 23:37:46 +0100

    add people pages

---
+++ astro/src/components/people/SubsiteSetter.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { currentSubsite, type SubsiteId } from '@/stores/subsiteStore';
+
+interface Props {
+  subsiteId: SubsiteId;
+}
+
+const props = defineProps<Props>();
+
+onMounted(() => {
+  currentSubsite.set(props.subsiteId);
+});
+</script>
+
+<template>
+  <span class="hidden" aria-hidden="true"></span>
+</template>
